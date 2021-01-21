650689276dc48ad4569f7ab4bf5ba8710bd588cf
François de Metz <francois.de-metz@pix.fr>
2021-01-21 11:39:38 +0100

    Use only a single loop to activate plugins (purchased or not)

--- bin/activate-plugins.sh
+++ bin/activate-plugins.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+if [[ -z "$MATOMO_LICENSE_KEY" ]]; then
+  echo "Not activating purchased plugins because no Matomo plugins API license key defined"
+else
+  if [[ -z "$MATOMO_PURCHASED_PLUGINS" ]]; then
+    echo "No purchased plugins specified. Did you forgotten to set MATOMO_PURCHASED_PLUGINS variable?"
+  else
+    if [ -z "$MATOMO_PLUGINS" ]; then
+        MATOMO_PLUGINS="$MATOMO_PURCHASED_PLUGINS"
+    else
+        MATOMO_PLUGINS="$MATOMO_PLUGINS,$MATOMO_PURCHASED_PLUGINS"
+    fi
+  fi
+fi
+
 if [[ "$MATOMO_PLUGINS" ]]; then
   IFS=',' read -ra plugins <<<"$MATOMO_PLUGINS"
 
@@ -9,26 +23,4 @@ if [[ "$MATOMO_PLUGINS" ]]; then
     echo "Activating $plugin_name"
     php console plugin:activate "$plugin_name"
   done
-fi
-
-echo "Activating Matomo purchased plugins..."
-
-if [[ -z "$MATOMO_LICENSE_KEY" ]]; then
-  echo "Do not fetch purchased plugins because no Matomo plugins API license key defined"
-else
-  if [[ -z "$MATOMO_PURCHASED_PLUGINS" ]]; then
-    echo "No purchased plugins specified. Did you forgotten to set MATOMO_PURCHASED_PLUGINS variable?"
-  else
-    plugins_dir="./plugins"
-
-    mkdir -p "${PWD}/$plugins_dir"
-
-    IFS=',' read -ra plugins <<<"$MATOMO_PURCHASED_PLUGINS"
-    for plugin in "${plugins[@]}"; do
-      plugin_name=${plugin%%:*}
-
-      echo "Activating $plugin_name"
-      php console plugin:activate "$plugin_name"
-    done
-  fi
 fi
